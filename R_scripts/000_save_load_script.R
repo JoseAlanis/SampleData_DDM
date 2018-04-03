08d5e324c624df81bb3061e935142c42beb6eb50
Jose Alanis <joialanisson@gmail.com>
2018-04-03 22:23:11 +0200

    fix paths

--- R_scripts/000_save_load_script.R
+++ R_scripts/000_save_load_script.R
@@ -6,18 +6,18 @@
 # ------ read in / write data to .RData / .csv and vice versa -------
 
 ## load .RData image
-load('./Documents/GitHub/SampleData_DDM/RData/sample_data.RData')
+load('./Documents/GitHub/SampleData_DDM/Data/sample_data.RData')
 ## save as .csv
-write.csv(sample_data, './Desktop/sample_data.csv', row.names = F)
+write.csv(sample_data, './Documents/GitHub/SampleData_DDM/Data/sample_data.csv', row.names = F)
 
 #rm(list = ls()) # clean Gloval Environment
 
 ## load .csv
-sample_data <- read.csv('./Desktop/sample_data.csv', 
+sample_data <- read.csv('./Documents/GitHub/SampleData_DDM/Data/sample_data.csv', 
                         header = T, 
                         colClasses = c('factor', 'factor', 'numeric', 
                                        'factor', 'factor', 'numeric', 'factor'))
 ## save .RData image
-save.image("~/Desktop/sample_data.RData")
+save.image("./Documents/GitHub/SampleData_DDM/Data/sample_data.RData")
 
 #rm(list = ls()) # clean Gloval Environment
